d5a278b7a748b4c3beecbd93e496ebc6d7082efd
andrewfarley2011 <andrewfarley@yahoo.com>
2017-08-21 23:17:24 -0400

    still wrking on it

--- app.rb
+++ app.rb
@@ -3,18 +3,31 @@ require 'sinatra'
 require_relative 'random_pairs.rb'
 
 get "/" do
-erb :index
+	erb :index
 end
 
 get '/pairs'do
-	names = params.values
-	pair = pair(names)
-erb :pairs, :locals => {pair: pair}
+	names = params[:names]
+	pair = params[:pair]
+	erb :pairs, :locals => {names: names, pair: pair}
 end
 
 post '/pairs' do
-	names = params.values
-	pair = pair(names)
-erb :pairs, :locals => {pair: pair}
+	names = params[:names]
+	pair = params[pair]
+	redirect '/confirm_pairs?names=' + names + '&pair=' + pairs
 end
+
+get '/confirm_pairs' do
+	names = params[:names]
+	pair = params[:pair]
+	erb :confirm_pairs, :locals => {pair: pair, names: names}		
+end
+
+post '/confirm_pairs' do
+	names = params[:names]
+	pair = params[:pair]
+	erb :confirm_pairs, :locals => {pair: pair, names: names}		
+end
+
 
